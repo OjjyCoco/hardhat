d233c1734fe824bc4871b6a3a2b64f4f6ff75b56
NathanWSL2 <nathanheckmann@gmail.com>
2025-02-24 12:57:06 +0100

    Increase Branch covering

--- test/voting.test.js
+++ test/voting.test.js
@@ -4,11 +4,11 @@ const { ethers } = require("hardhat");
 
 describe("Voting Contract", function () {
     async function deployContract() {
-        const [owner, voter1, voter2] = await ethers.getSigners();
+        const [owner, voter1, voter2, voter3] = await ethers.getSigners();
         const Voting = await ethers.getContractFactory("Voting");
         const voting = await Voting.deploy();
 
-        return { voting, owner, voter1, voter2 };
+        return { voting, owner, voter1, voter2, voter3 };
     }
 
     describe("Deployment", function () {
@@ -56,9 +56,19 @@ describe("Voting Contract", function () {
             ({ voting, owner, voter1 } = await loadFixture(deployContract));
         });
 
+        it("Should revert because wrong WorkflowStatus for startProposalsRegistering", async function () {
+            await voting.startProposalsRegistering();
+            await voting.endProposalsRegistering();
+            await expect(voting.startProposalsRegistering()).to.be.reverted;
+        });
+
+        it("Should revert because wrong WorkflowStatus for endProposalsRegistering", async function () {
+            await expect(voting.endProposalsRegistering()).to.be.reverted;
+        });
+
         it("Should start proposals registration", async function () {
             await voting.startProposalsRegistering();
-            expect(await voting.workflowStatus()).to.equal(1); // ProposalsRegistrationStarted
+            expect(await voting.workflowStatus()).to.equal(1);
         });
 
         it("Should register a proposal", async function () {
@@ -89,11 +99,31 @@ describe("Voting Contract", function () {
             await voting.startProposalsRegistering();
             await expect(voting.connect(voter1).addProposal("")).to.be.reverted;
         });
+
+        it("Should create the GENESIS proposal on startProposalsRegistering WF status", async function () {
+            await voting.addVoter(voter1.address);
+            await voting.startProposalsRegistering();
+            const proposal = await voting.connect(voter1).getOneProposal(0); // Résolution de la promesse
+            await expect(proposal.description).to.equal("GENESIS");
+        });
+
     });
 
     describe("Voting", function () {
+        let voting, owner, voter1;
+        beforeEach(async function () {
+            ({ voting, owner, voter1 } = await loadFixture(deployContract));
+        });
+
+        it("Should revert because wrong WorkflowStatus for startVotingSession", async function () {
+            await expect(voting.startVotingSession()).to.be.reverted;
+        });
+
+        it("Should revert because wrong WorkflowStatus for endVotingSession", async function () {
+            await expect(voting.endVotingSession()).to.be.reverted;
+        });
+
         it("Should emit an event when a voter has voted", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
             await voting.addVoter(voter1.address);
             await voting.startProposalsRegistering();
             await voting.connect(voter1).addProposal("Proposal 1");
@@ -104,7 +134,6 @@ describe("Voting Contract", function () {
         });
 
         it("Should allow registered voters to vote", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
             await voting.addVoter(voter1.address);
             await voting.startProposalsRegistering();
             await voting.connect(voter1).addProposal("Proposal 1");
@@ -116,13 +145,11 @@ describe("Voting Contract", function () {
         });
         
         it("Should revert when voting before session starts", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
             await voting.addVoter(voter1.address);
             await expect(voting.connect(voter1).setVote(0)).to.be.reverted;
         });
 
         it("Should revert when voter has already voted", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
             await voting.addVoter(voter1.address);
             voting.connect(voter1).setVote(0)
             await expect(voting.connect(voter1).setVote(0)).to.be.reverted;
@@ -145,14 +172,6 @@ describe("Voting Contract", function () {
             await expect(voting.tallyVotes())
             .to.emit(voting, 'WorkflowStatusChange');
             expect(await voting.workflowStatus()).to.equal(5); // VotesTallied
-        });
-
-        it("Should create the GENESIS proposal on startProposalsRegistering WF status", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
-            await voting.addVoter(voter1.address);
-            await voting.startProposalsRegistering();
-            const proposal = await voting.connect(voter1).getOneProposal(0); // Résolution de la promesse
-            expect(proposal.description).to.equal("GENESIS");
         });
 
         // Honnêtement un peu la flemme de tester tous les require de tous les changement de status
@@ -180,9 +199,31 @@ describe("Voting Contract", function () {
             expect(await voting.winningProposalID()).to.equal(0);
         });
 
-        it("Should revert when tallying when the wrong WorkflowStatus is on", async function () {
-            const { voting, voter1 } = await loadFixture(deployContract);
+        it("Should determine the winning proposal (the second is the winner)", async function () {
+            const { voting, voter1, voter2, voter3 } = await loadFixture(deployContract);
             await voting.addVoter(voter1.address);
+            await voting.addVoter(voter2.address);
+            await voting.addVoter(voter3.address);
+            await voting.startProposalsRegistering();
+            await voting.connect(voter1).addProposal("Proposal 1");
+            await voting.connect(voter1).addProposal("Proposal 2");
+            await voting.endProposalsRegistering();
+            await voting.startVotingSession();
+            await voting.connect(voter1).setVote(0);
+            await voting.connect(voter2).setVote(1);
+            await voting.connect(voter3).setVote(1);
+            await voting.endVotingSession();
+            await expect(voting.tallyVotes())
+            .to.emit(voting, 'WorkflowStatusChange');
+            // No draw, the winner is Proposal 2
+            expect(await voting.winningProposalID()).to.equal(1);
+        });
+
+        // Pas besoin de faire le cas : Should determine the winning proposal (the first is the winner)
+        // Car les mêmes branch sont testées par Should determine the winning proposal (the first when draw)
+
+        it("Should revert because wrong WorkflowStatus for tallyVotes", async function () {
+            const { voting, voter1 } = await loadFixture(deployContract);
             await expect(voting.tallyVotes()).to.be.reverted;
         });
 
